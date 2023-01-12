5eac121c4cc9338da5adeaf9c6d64e4234607144
Adam Oswald <adamoswald69420@gmail.com>
2023-01-12 11:58:00 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,114 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from element_manager import *
+from selenium.webdriver.support.ui import Select
+driver = webdriver.Chrome(ChromeDriverManager().install())
+# to click on the element(Improving Vocabulary...) found
+driver.find_element(By.XPATH,get_xpath(driver,'d3SIYrAn_YsVnul')).click()
+
+# to click on the element(Improving Vocabulary...) found
+driver.find_element(By.XPATH,get_xpath(driver,'P3EPcZpbdgyCfTm')).click()
+
+# to click on the element(window.NREUM||(NREUM...) found
+driver.find_element(By.XPATH,get_xpath(driver,'RzFcwWlGNeggAC5')).click()
+
+# to click on the element(Closed Captioning) found
+driver.find_element(By.XPATH,get_xpath(driver,'SQEscWYvQiyVIVP')).click()
+
+# to click on the element(Closed Captioning) found
+driver.find_element(By.XPATH,get_xpath(driver,'a_ocaL9zLW6eaBC')).click()
+
+# to click on the element found
+driver.find_element(By.XPATH,get_xpath(driver,'TZbfJcEYduR_g26')).click()
+
+# to click on the element(Glossary) found
+driver.find_element(By.XPATH,get_xpath(driver,'gsqufh9FbIxXB4B')).click()
+
+# to click on the element(Transcript) found
+driver.find_element(By.XPATH,get_xpath(driver,'9HYbYL8_0sHOWK2')).click()
+
+# to click on the element found
+driver.find_element(By.XPATH,get_xpath(driver,'eOnaKA9pBXmf8cq')).click()
+
+# to click on the element(Tweaks) found
+driver.find_element(By.XPATH,get_xpath(driver,'oTYIWku1Rls4ZQb')).click()
+
+# to click on the element(Skip intro) found
+driver.find_element(By.XPATH,get_xpath(driver,'47xwzfd2xM0LVoV')).click()
+
+# to click on the element(Skip intro) found
+driver.find_element(By.XPATH,get_xpath(driver,'29gq8myWv7FlgQI')).click()
+
+# to click on the element(Tweaks) found
+driver.find_element(By.XPATH,get_xpath(driver,'nBrivvPkaqtcovh')).click()
+
+# to click on the element(Improving Vocabulary...) found
+driver.find_element(By.XPATH,get_xpath(driver,'3y1Wh5nix3Y_fcS')).click()
+
+Loading...
+
+Loading...
+
+Loading...
+
+Loading...
+
+Loading...
+
+Loading...
+
+# to click on the element(Brent Jacobs) found
+driver.find_element(By.XPATH,get_xpath(driver,'b9GRMdefJsMgK0Y')).click()
+
+# to click on the element(Go Right) found
+driver.find_element(By.XPATH,get_xpath(driver,'IA_HqvarMz4sPF4')).click()
+
+# to click on the element(Go Right) found
+driver.find_element(By.XPATH,get_xpath(driver,'xGp0rkLef3IUOtc')).click()
+
+Loading...
+
+# to select the option
+select_element=driver.find_element(By.XPATH,get_xpath(driver,'f8hkBnUuIdDIyip'))
+selecttag=Select(select_element)
+selecttag.select_by_value('296589')
+
+# to select the option
+select_element=driver.find_element(By.XPATH,get_xpath(driver,'lhSu7kf28vTXfpY'))
+selecttag=Select(select_element)
+selecttag.select_by_value('296592')
+
+# to select the option
+select_element=driver.find_element(By.XPATH,get_xpath(driver,'9b5w6juAhH9u6xO'))
+selecttag=Select(select_element)
+selecttag.select_by_value('296594')
+
+# to select the option
+select_element=driver.find_element(By.XPATH,get_xpath(driver,'b8lRN_d86i0HsuS'))
+selecttag=Select(select_element)
+selecttag.select_by_value('296598')
+
+# to click on the element(Go Right) found
+driver.find_element(By.XPATH,get_xpath(driver,'_VNTxQncncIdkVy')).click()
+
+# to select the option
+select_element=driver.find_element(By.XPATH,get_xpath(driver,'_8zz2zGE_k6Dk9m'))
+selecttag=Select(select_element)
+selecttag.select_by_value('296595')
+
+# to click on the element(Go Right) found
+driver.find_element(By.XPATH,get_xpath(driver,'dmid_SnK3qFFvct')).click()
+
+# to click on the element found
+driver.find_element(By.XPATH,get_xpath(driver,'fwIVmgcN8_ygxgG')).click()
+
+# to click on the element(Go Right) found
+driver.find_element(By.XPATH,get_xpath(driver,'g4m2NKxHLmhSb2u')).click()
+
+# to click on the element(Go Right) found
+driver.find_element(By.XPATH,get_xpath(driver,'x_JrGS4zId8Trml')).click()
+
+# to click on the element(Go Right) found
+driver.find_element(By.XPATH,get_xpath(driver,'QlCWdxbIdr3iKmp')).click()
+
